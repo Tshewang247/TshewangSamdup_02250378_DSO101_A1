11ea27d86856154e584a8cb212e0b711b8eef766
Tshewang Samdup <02250378.cst@rub.edu.bt>
2026-06-09 18:21:06 +0600

    Add jest tests and build script

--- Assignment1/todo-app/backend/tests/todo.test.js
+++ Assignment1/todo-app/backend/tests/todo.test.js
@@ -2,4 +2,10 @@ describe('Todo API', () => {
   test('basic test passes', () => {
     expect(1 + 1).toBe(2);
   });
+
+  test('todo item has required fields', () => {
+    const todo = { title: 'Buy milk', completed: false };
+    expect(todo.title).toBeDefined();
+    expect(todo.completed).toBe(false);
+  });
 });
